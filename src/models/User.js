92d3f5bdd4a3921179141818836f44803a13f3f0
Davejs136 <davejs136@gmail.com>
2019-04-21 19:48:15 -0400

    src/auth/passport.js: Codigo de autenticacion, hubieron otros cambios, pero son cambios menores!

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,13 @@
 // 1- Destructuring del obj mongoose
 // 2- Creo el Schema del User
 // 3- se crea el modelo y se exporta
-const { model, Schema } = require('mongoose'),
- 
-userSchema = 
-  new Schema({
-    name: String,
-    lastname: String,
-    email: String,
-    pwd: String
-  }), 
-  
-User = model('User', userSchema);
+const { model, Schema } = require('mongoose')
+  , userSchema = new Schema({
+          name: String
+        , lastname: String
+        , email: String
+        , pwd: String
+      })
+  , User = model('User', userSchema);
 
 module.exports = User;
