6ea856f73d54c0a983bf3abc511b7b018652dff7
David <davejs136@gmail.com>
2019-04-22 16:01:57 -0400

    cambios menores (nombres, rutas)

--- src/models/User.js
+++ src/models/User.js
@@ -2,12 +2,30 @@
 // 2- Creo el Schema del User
 // 3- se crea el modelo y se exporta
 const { model, Schema } = require('mongoose')
-  , userSchema = new Schema({
-          name: String
-        , lastname: String
-        , email: String
-        , pwd: String
-      })
-  , User = model('User', userSchema);
+    , { genSaltSync, compareSync, hashSync } = require('bcrypt')
 
-module.exports = User;
+let userSchema, userModel
+
+userSchema = new Schema({
+    name: String
+  , lastname: String
+  , email: String
+  , password: String
+})
+
+// Metodos
+userSchema.methods.encryptPassword = password => {
+  // retorna la contraseña encryptada
+  return hashSync(password, genSaltSync(10))
+}
+
+// Se usa function() para poder hacer referencia a "this" de userschema
+userSchema.methods.comparePassword = function( password ) {
+  // "this.password" accede a la contraseña
+  // que esta en el obj
+  return compareSync(password, this.password)
+}
+
+userModel = model('User', userSchema)
+
+module.exports = userModel;
