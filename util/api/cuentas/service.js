1e1867f9ce2bc417a9756835d962f7a60a207230
Kuudev <kuuhakudeveloper@outlook.com>
2021-01-18 11:43:14 -0600

    add: get list to accounts

--- util/api/cuentas/service.js
+++ util/api/cuentas/service.js
@@ -5,20 +5,36 @@ import {reply} from "../reply"
 
 export async function add(token, data){    
 
-    try {
-        await dbConnect()
-        data.userId = (await getIdbyToken(token)).userId;
-        console.log(typeof data.userId)
-        const account = await Account.create(
-          data
-        ) /* create a new model in the database */
-        console.log("Agregado: " + account)
-        reply.reply = account;
-        reply.msg = "Cuenta creada con exito."
-        return reply
-      } catch (error) {
-        console.log("No se pudo por no se que: " + error)
-        reply.msg = "Ocurrio un error al intentar crear la cuenta."
-        return reply
-      }
+  try {
+      await dbConnect()
+      data.userId = (await getIdbyToken(token)).userId;
+      const account = await Account.create(
+        data
+      ) /* create a new model in the database */
+      console.log("Agregado: " + account)
+      reply.reply = account;
+      reply.msg = "Cuenta creada con exito."
+      return reply
+    } catch (error) {
+      console.log("No se pudo por no se que: " + error)
+      reply.msg = "Ocurrio un error al intentar crear la cuenta."
+      return reply
+    }
+}
+
+export async function get(token){    
+
+  try {
+      await dbConnect()
+      let userId = (await getIdbyToken(token)).userId;
+      const accounts = await Account.find({userId: userId}).exec(); /* create a new model in the database */
+      console.log("Lista generada: \n" + accounts)
+      reply.reply = accounts;
+      reply.msg = "Cuentas obtenidas con exito."
+      return reply
+    } catch (error) {
+      console.log("No se pudo por no se que: " + error)
+      reply.msg = "Ocurrio un error al intentar crear la cuenta."
+      return reply
+    }
 }
